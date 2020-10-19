0ddba2afa0d2619d8ccb24e94cfeddadf008f063
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-10-19 10:34:20 -0300

    preparando components necessarios

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
-  <div id="app">
+<div id="app">
 
     <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="dislplay-4">Requisições HTTP no Vue</h1>
-        <p class="lead">
-          Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST
-        </p>
-      </div>
+        <div class="container">
+            <h1 class="display-4">Requisições HTTP no Vue</h1>
+            <p class="lead">Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.</p>
+        </div>
     </div>
 
     <div class="container">
 
+        <TarefasLista />
+
     </div>
 
-  </div>
+</div>
 </template>
 
 <script>
+import TarefasLista from './components/TarefasLista.vue'
 
 export default {
-  name: "App"
-};
+    components: {
+        TarefasLista
+    }
+}
 </script>
