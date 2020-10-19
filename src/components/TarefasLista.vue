e821bf95dff359f41c74501f35981e9409687a4b
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-10-19 13:51:58 -0300

    requisicao delete para excluir tarefas existentes

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -13,7 +13,7 @@
     </div>
 
     <ul class="list-group" v-if="tarefas.length > 0">
-        <TarefasListaIten v-for="tarefa in tarefas" :key="tarefa.id" :tarefa="tarefa" @editar="selecionarTarefaParaEdicao" />
+        <TarefasListaIten v-for="tarefa in tarefas" :key="tarefa.id" :tarefa="tarefa" @editar="selecionarTarefaParaEdicao" @deletar="deletarTarefa" />
     </ul>
 
     <p v-else>Nenhuma tarefa criada.</p>
@@ -43,7 +43,7 @@ export default {
     },
     created() {
         axios.get(`${config.apiUrl}/tarefas`).then(response => {
-            console.log("POST / tarefas", response);
+            console.log("POST / tarefas", response.data);
             this.tarefas = response.data;
         });
     },
@@ -65,6 +65,22 @@ export default {
                     this.tarefas.splice(indice, 1, tarefa);
                     this.resetar();
                 });
+        },
+        deletarTarefa(tarefa) {
+            const confirmar = window.confirm(
+                `Deseja deletar a tarefa ${tarefa.titulo} ?`
+            );
+
+            if (confirmar) {
+                axios.delete(`${config.apiUrl}/tarefas/${tarefa.id}`).then(response => {
+                    console.log(`DELETE /tarefas/${tarefa.id}`, response);
+                    const indice = this.tarefas.findIndex(t => t.id === tarefa.id);
+                    this.tarefas.splice(indice, 1, tarefa);
+                    this.resetar();
+
+                    location.reload();
+                });
+            }
         },
         resetar() {
             this.tarefaSelecionada = undefined;
--- src/components/TarefasListaIten.vue
+++ src/components/TarefasListaIten.vue
@@ -10,7 +10,7 @@
         <i class="fa fa-pencil-alt"></i>
     </button>
 
-    <button class="btn btn-danger btn-sm" title="Deletar">
+    <button class="btn btn-danger btn-sm" title="Deletar" @click="$emit('deletar', tarefa)">
         <i class="fa fa-trash"></i>
     </button>
 </li>
