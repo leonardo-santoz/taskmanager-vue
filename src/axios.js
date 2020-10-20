9110c7003c4d73c8d41843ab7708c2baaeda09a3
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-10-20 16:01:30 -0300

    interceptando requisicoes

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,30 @@
-import axios from 'axios';
-import config from './config/config';
+import axios from "axios";
+import config from "./config/config";
 
-const instance = axios.create({})
+const instance = axios.create({});
 
 instance.defaults.baseURL = config.apiUrl;
+
+instance.interceptors.request.use(
+  config => {
+    console.log("interceptando requisicao:", config);
+    config.data = {
+      ...config.data,
+      curso: "VueJS"
+    };
+
+    return new Promise((resolve, reject) => {
+      console.log("Aguardando requisicao...");
+      setTimeout(() => {
+        console.log("Enviando requisicao...");
+        resolve(config);
+      }, 2000);
+    });
+  },
+  error => {
+    console.log("erro ao fazer requisicao", error);
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
